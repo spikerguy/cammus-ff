a4f6926eb5487dfc99c8db3956020f29370c0394
spikerguy <48839029+spikerguy@users.noreply.github.com>
2024-02-12 20:06:03 +0300

    use cammus c5 identifiers

--- hid-simagic.h
+++ hid-simagic.h
@@ -2,8 +2,8 @@
 #ifndef __HID_SIMAGIC_H
 #define __HID_SIMAGIC_H
 
-#define USB_VENDOR_ID_SIMAGIC		0x0483
-#define USB_DEVICE_ID_SIMAGIC_WHEEL 0x0522
+#define USB_VENDOR_ID_SIMAGIC		0x3416
+#define USB_DEVICE_ID_SIMAGIC_WHEEL 0x0301
 
 int hid_pidff_init_simagic(struct hid_device *hdev);
 
